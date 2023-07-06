d1c756f0fd8a917f9175c4352491bed8805dd201
Boris Damevin <borisdamevin@gmail.com>
2023-07-07 01:09:12 +0200

    feat: update docs

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -57,7 +57,7 @@ export default defineConfig({
         socialLinks: [
             {
                 icon: 'github',
-                link: 'https://github.com/vuejs/vitepress'
+                link: 'https://github.com/magnesiumlabs/magnesium'
             }
         ],
         footer: {
